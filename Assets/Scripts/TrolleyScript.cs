098c992a74ace6fbe449ad35878168392c179503
Haaksi <126588878+Haaksi@users.noreply.github.com>
2024-10-24 12:55:11 +0300

    Last try on Exercise 1
    Couldn't get the slider to work, but the trolley and crane move with arrow keys.

--- Assets/Scripts/TrolleyScript.cs
+++ Assets/Scripts/TrolleyScript.cs
@@ -1,48 +1,106 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TrolleyScript : MonoBehaviour
 {
-    [SerializeField] GameObject crane;
-    [SerializeField] GameObject nearLimit;
-    [SerializeField] GameObject farLimit;
+    [SerializeField] GameObject crane;      // Reference to crane
+    [SerializeField] GameObject nearLimit;  // Reference to near limit
+    [SerializeField] GameObject farLimit;   // Reference to far limit
+    [SerializeField] float tMoveSpeed;      // Move speed of trolley
 
-    private Vector3 tRelatPos;
-    private Vector3 tRelatAxisY;
-    private Vector3 tRelatAxisZ;
+    [SerializeField] Slider trolleySlider;  // Reference to Trolley Slider
+
+    public Vector3 tRelatPos;               // Relative position of the trolley to the crane
+    public Vector3 tRelatRot;               // Relative rotation of the trolley to the crane
+    private Vector3 tMovement;               // Current movement of the trolley
+
+    private float minLimit;                 // Minimum limit from the crane
+    private float maxLimit;                 // Maximum limit from the crane
 
     private void Awake()
     {
-        tRelatPos = crane.transform.InverseTransformVector(transform.position - crane.transform.position);
-        tRelatAxisY = crane.transform.InverseTransformVector(transform.up);
-        tRelatAxisZ = crane.transform.InverseTransformVector(transform.forward);
+        if (crane != null)
+        {
+            tRelatPos = transform.position - crane.transform.position;
+            tRelatRot = transform.eulerAngles - crane.transform.eulerAngles;
+            tMovement = Vector3.zero;
+
+            SetLimits();
+        }
+
+        if (trolleySlider != null)
+        {
+            trolleySlider.value = GetCurrentSliderValue();
+        }
     }
 
     private void Update()
     {
-        transform.position = crane.transform.position + crane.transform.TransformVector(tRelatPos);
-        //transform.position = trueRelativePos();
-        transform.rotation = Quaternion.LookRotation(crane.transform.TransformVector(tRelatAxisZ), crane.transform.TransformVector(tRelatAxisY));
+        TrolleyMovement();
     }
 
-    Vector3 trueRelativePos()
+    private void LateUpdate()
     {
-        var posY = crane.transform.position.y + crane.transform.TransformVector(tRelatPos).y;
-        var posZ = crane.transform.position.z + crane.transform.TransformVector(tRelatPos).z;
-
-        return new Vector3(transform.position.x, posY, posZ);
-    }
-
-    void posCheck()
-    {
-        if (Mathf.Abs(Vector3.Distance(transform.position, farLimit.transform.position)) <= 0)
+        if (crane != null)
         {
-            transform.position = new Vector3(farLimit.transform.position.x, transform.position.y, transform.position.z);
+            // Calculate new position by applying the offset in world space
+            Vector3 newPosition = crane.transform.position + crane.transform.TransformDirection(tRelatPos + tMovement);
+            newPosition.y = transform.position.y; // Keep the Y position the same
+
+            // Calculate current distance from crane to new position
+            float currentDistance = Vector3.Distance(crane.transform.position, newPosition);
+
+            // Check if the new position exceeds the limits
+            if (currentDistance < minLimit || currentDistance > maxLimit)
+            {
+                // Calculate how much movement to apply to keep it within limits
+                float distanceToLimit = currentDistance < minLimit ? minLimit - currentDistance : currentDistance - maxLimit;
+
+                // Adjust tMovement based on the distance to limit
+                tMovement += new Vector3(-distanceToLimit * Mathf.Sign(tMovement.x), 0, 0);
+            }
+
+            // Update the position of the trolley
+            transform.position = newPosition;
+
+            // Apply rotation offset to maintain relative rotation
+            Quaternion craneRotation = Quaternion.Euler(crane.transform.eulerAngles);
+            Quaternion trolleyRotation = Quaternion.Euler(tRelatRot);
+            transform.rotation = craneRotation * trolleyRotation;
         }
-        if (Mathf.Abs(Vector3.Distance(transform.position, nearLimit.transform.position)) <= 0)
+    }
+
+    void TrolleyMovement()
+    {
+        float tMoveX = Input.GetAxis("Vertical") * tMoveSpeed * Time.deltaTime;
+        float sliderValue = trolleySlider.value;
+
+        // Calculate the proposed new position with movement
+        Vector3 proposedNewPosition = transform.position + crane.transform.TransformDirection(new Vector3(tMoveX, 0, 0));
+        float currentDistance = Vector3.Distance(crane.transform.position, proposedNewPosition);
+
+        // Only allow movement if within limits
+        if (currentDistance >= minLimit && currentDistance <= maxLimit)
         {
-            transform.position = new Vector3(nearLimit.transform.position.x, transform.position.y, transform.position.z);
+            tMovement += new Vector3(tMoveX, 0, 0); // Update movement if within limits
+        }
+    }
+
+    float GetCurrentSliderValue()
+    {
+        // Get the current distance and map it to the slider value
+        float currentDistance = Vector3.Distance(crane.transform.position, transform.position);
+        return Mathf.InverseLerp(minLimit, maxLimit, currentDistance);
+    }
+
+    void SetLimits()
+    {
+        if (nearLimit != null && farLimit != null)
+        {
+            minLimit = Vector3.Distance(crane.transform.position, nearLimit.transform.position);
+            maxLimit = Vector3.Distance(crane.transform.position, farLimit.transform.position);
         }
     }
 }
